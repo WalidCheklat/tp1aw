511ff89ded46f6f099cdb8f5d645a53f8bb3da81
WalidCheklat <56535361+WalidCheklat@users.noreply.github.com>
2019-11-04 12:03:24 +0100

    Update formulaire.js

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,17 +1,21 @@
 function validation() {
+
+    document.getElementById("error").innerHTML ="";
+    document.getElementById("error").style.visibility ="";
+
     if(document.getElementById("Nom").value.length<5){
  document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
     }    
-else if(document.getElementById("Prénom").value.length<5){
+    if(document.getElementById("Prénom").value.length<5){
  document.getElementById("error").innerHTML = "La saisie du prénom est obligatoire";
     }
-else if(document.getElementById("Adresse").value.length<5){
+ if(document.getElementById("Adresse").value.length<5){
  document.getElementById("error").innerHTML = "La saisie de l'adresse est obligatoire";
 }
-else if(document.getElementById("Mail").value.length<5){
+ if(document.getElementById("Mail").value.length<5){
  document.getElementById("error").innerHTML = "La saisie du mail est obligatoire";
 }
-else{
+if{
  document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
 }
 }
