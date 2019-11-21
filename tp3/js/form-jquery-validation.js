e9605cbdaf38f07875d216c5550907a5d06c08f0
WalidCheklat <56535361+WalidCheklat@users.noreply.github.com>
2019-11-21 20:29:09 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -5,34 +5,43 @@ $( document ).ready(function() {
 
     $("form").on("submit", function (event) {
         event.preventDefault();
-  
+
+        var a = "https://maps.googleapis.com/maps/api/staticmap?markers="+ $("#adresse").val()+ "&zoom=10&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg";
+        var b = 0;
+
    if ($("#nom").val()==""){
     $(".modal-body").text("Veuillez saisir votre nom");
     $('#myModal').modal("show");
+    b = b+1;
     }
     
    if ($("#prénom").val()==""){
     $(".modal-body").text("Veuillez saisir votre prénom");
     $('#myModal').modal("show");
+    b = b+1;
     }
 
    else if ($("#date").val()==""){
     $(".modal-body").text("Veuillez saisir votre date de naissance");
     $('#myModal').modal("show");
+    b = b+1;
     }
 
    else if ($("#email").val()==""){
     $(".modal-body").text("Veuillez saisir votre email");
     $('#myModal').modal("show");
+    b = b+1;
     }
 
    else if($("#adresse").val()==""){
     $(".modal-body").text("Veuillez saisir votre adresse");
     $('#myModal').modal("show");
+    b = b+1;
     }
 
     else {
-    $(".modal-body").text("Bienvenu");
+    $(".modal-title").text("Bienvenu " + $("#prénom").val());
+    $(".modal-body").html("Vous êtes né le " + $("#date").val() + " et vous habitez à " + "(" + $("#adresse").val() + ")" + '<img src="'+a+'" width = "400" height = "300" />');
     $('#myModal').modal("show");
     }
 
