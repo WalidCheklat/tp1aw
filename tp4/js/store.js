67d6a593a479f3261472f5b02b400d7716eae78f
WalidCheklat <56535361+WalidCheklat@users.noreply.github.com>
2019-12-05 14:51:07 +0100

    Update store.js

---
+++ tp4/js/store.js
@@ -0,0 +1,20 @@
+var contactStore = (function () {
+    var contactList = [];
+    return {
+        add: function(_name, _firsname, _date, _adress, _mail) {
+          var contact = { name: _name,
+                          firstname: _firsname,
+                          date: _date,
+                          adress: _adress,
+                          mail: _mail
+          };
+          contactList.push(contact);
+          return contactList;
+        },
+    
+        getList: function() {
+          return contactList;
+        }
+      }
+    })();
+    
